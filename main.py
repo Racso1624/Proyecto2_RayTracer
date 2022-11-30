2f181976cde1be09f1c5bab0976dd2ce8c6ee7e8
Racso1624 <fer10_lb@outlook.com>
2022-11-30 17:36:30 -0600

    Se agregan elementos

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ red_ball = Material(difusse=Color(255, 0, 0), albedo = [1, 0.1, 0.48, 0], spec =
 yellow_ball = Material(difusse=Color(249,236,12), albedo = [1, 0.1, 0.48, 0], spec = 50, refractive=50) 
 white = Material(difusse=Color(255, 255, 255), albedo = [1.3, 0.1, 0, 0], spec = 40, refractive=20)
 black = Material(difusse=Color(0, 0, 0), albedo = [1.3, 0.1, 0, 0], spec = 50, refractive=20)
-silver = Material(difusse=Color(192, 192, 192), albedo = [1.3, 0.1, 0, 0], spec = 50, refractive=20)
-light_brown = Material(difusse=Color(204, 102, 53), albedo = [1.3, 0.1, 0.2, 0], spec = 50, refractive=50)
-apricot = Material(difusse=Color(242, 178, 140), albedo = [1.3, 0.1, 0, 0], spec = 30, refractive=20)
+light_brown = Material(difusse=Color(204, 102, 53), albedo = [1, 0.2, 0.2, 0], spec = 50, refractive=50)
+apricot = Material(difusse=Color(242, 178, 140), albedo = [1, 0.1, 0.3, 0], spec = 50, refractive=50)
 tree_leaves = Material(difusse=Color(2,105,57), albedo=[1, 0.1, 0, 0], spec=50, refractive=50)
 tree_log = Material(difusse=Color(108,66,39), albedo=[1, 0.1, 0, 0], spec=50, refractive=50)
 
@@ -45,9 +44,13 @@ r.scene = [
     Sphere(V3(-4.3, 2.3, -8), 0.25, red_ball),
 
     #Piso
-    Plane(V3(3.5, 5, -11), 3, 6, light_brown),
+    Plane(V3(3.3, 5, -11), 3, 6, light_brown),
     Plane(V3(0, 5, -11), 3, 6, light_brown),
-    Plane(V3(-3.5, 5, -11), 3, 6, light_brown),
+    Plane(V3(-3.3, 5, -11), 3, 6, light_brown),
+
+    #Regalo
+    Cube(V3(1, 1.5, -4), 0.2, red_ball),
+    Cube(V3(1.5, 2.2, -5), 0.5, apricot),
 
 ]
 r.render()
